7743a1f76d766eb830c5f7fff85fc25027d07ff6
Yukusai <85849071+p-yukusai@users.noreply.github.com>
2021-09-16 06:33:49 +0500

    Fix for context_menu limitation

--- src/shellHandler.py
+++ src/shellHandler.py
@@ -2,9 +2,8 @@ import os
 import pathlib
 from context_menu import menus
 
+
 # Having to define stuff anew in this script, since it's technically separate in the context of the shell #
-
-
 def documentFolder():
     import ctypes.wintypes
     doc = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
@@ -16,8 +15,6 @@ def documentFolder():
 def getScriptFolder():
     document_folder = documentFolder()
     return document_folder + "\\PyWall\\Executable.txt"
-
-
 # Making the messagebox in Qt saves space in the executable, otherwise we would have the good old Tkinter bloat #
 def pop(title, text, close: bool):
     import sys
@@ -42,9 +39,8 @@ def pop(title, text, close: bool):
 
     window()
 
+
 # The "open" command is repeated because I'm too lazy to define it and then just call it later, code redundancy go brr #
-
-
 def allowAccess(filenames, params):
     try:
         with open(getScriptFolder(), 'r') as sf:
@@ -88,7 +84,6 @@ def denyAccess(filenames, params):
 # This creates the context menu, It is important to do this for FILES and for DIRECTORY so that the user can right #
 # click on either. The keys created are in "HKEY_CURRENT_USER\Software\Classes\*\shell\" for FILES and in #
 # "HKEY_CURRENT_USER\Software\Classes\Directory\shell" for DIRECTORY #
-
 def createInternetAccessMenu():
     IAM = menus.ContextMenu('PyWall', type='FILES')
     IAM.add_items([menus.ContextCommand('Allow Internet Access', python=allowAccess),
@@ -99,6 +94,39 @@ def createInternetAccessMenu():
     IAM_Folder.add_items([menus.ContextCommand('Allow Internet Access', python=allowAccess),
                           menus.ContextCommand('Deny Internet Access', python=denyAccess)])
     IAM_Folder.compile()
+
+    # This is a to bypass a limitation of context_menu, or rather, of the "Run" command. #
+    # In brief, the run command can only be a certain length, so we're removing some of the text from the handler #
+    # and replacing it with a custom parser in "main.py" #
+    
+    import winreg
+
+    IAM_FILES_ALLOW = r"Software\Classes\*\shell\PyWall\shell\Allow Internet Access\command"
+    IAM_FILES_DENY = r"Software\Classes\*\shell\PyWall\shell\Deny Internet Access\command"
+    IAM_DIR_ALLOW = r"Software\Classes\Directory\shell\PyWall\shell\Allow Internet Access\command"
+    IAM_DIR_DENY = r"Software\Classes\Directory\shell\PyWall\shell\Deny Internet Access\command"
+    key = winreg.HKEY_CURRENT_USER
+    sub_keys = [IAM_FILES_ALLOW, IAM_FILES_DENY, IAM_DIR_ALLOW, IAM_DIR_DENY]
+
+    for x in sub_keys:
+        current_sub_key = winreg.QueryValue(key, x)
+        argIndex = winreg.QueryValue(key, x).index(" -c ")
+        # About the dumbest way to query for the fourth semi-colon #
+        firstSemi = current_sub_key.find(";")
+        secondSemi = current_sub_key.find(";", firstSemi + 1)
+        thirdSemi = current_sub_key.find(";", secondSemi + 1)
+        print(thirdSemi)
+        if "shellHandler.allowAccess" in current_sub_key:
+            accessIndex = current_sub_key.find("shellHandler.allowAccess") + 24
+        else:
+            accessIndex = current_sub_key.find("shellHandler.denyAccess") + 23
+        quoteIndex = current_sub_key.find('"', thirdSemi)
+        print(quoteIndex)
+
+        replacement_sub_key = current_sub_key[:argIndex + 4] + current_sub_key[thirdSemi + 1: accessIndex] + \
+                              current_sub_key[quoteIndex:]
+        print(replacement_sub_key)
+        winreg.SetValue(key, x, winreg.REG_SZ, replacement_sub_key)
 
 
 def removeInternetAccessMenu():
